70c08fce3e3c87e0d48f212b18900655f60f5233
Mark Lennon <mplennon@gmail.com>
2010-10-22 12:20:42 +1000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Calm1::Application.config.secret_token = 'cfd9cdc1d300f9a213cc3789ddd68c35920ee32422659c8508aa5b5b96f6e5ecafeac9dde08ed4402cd8766f6fa922ad2d09635f078019581dcb87e62102c162'
